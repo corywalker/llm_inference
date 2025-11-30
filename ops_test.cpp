efad8966af679e144a19fee9d63ab3b11c2b94a7
Cory Walker <cwalker32@gmail.com>
2025-11-29 23:26:40 -0800

    Basic tests for rope and scale.

--- ops_test.cpp
+++ ops_test.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "gtest/gtest.h"
+#include "tensor.h"
 
 bool verbose_g = false;
 
@@ -44,4 +45,35 @@ TEST(OpsTest, Softmax) {
   EXPECT_NEAR(x[1], expf(2.0 - max_val) / sum, 1e-6);
   EXPECT_NEAR(x[2], expf(3.0 - max_val) / sum, 1e-6);
   EXPECT_NEAR(x[3], expf(4.0 - max_val) / sum, 1e-6);
+}
+
+TEST(OpsTest, Rope) {
+  tensor_3 t = {{{1.0f, 2.0f, 3.0f, 4.0f}}};
+  int n_rot = 4;
+  float rope_freq_base = 10000.0f;
+  float rope_freq_scale = 1.0f;
+  int pos = 0;
+
+  rope(t, n_rot, rope_freq_base, rope_freq_scale, pos);
+
+  EXPECT_NEAR(t[0][0][0], 1.0f, 1e-6);
+  EXPECT_NEAR(t[0][0][1], 2.0f, 1e-6);
+  EXPECT_NEAR(t[0][0][2], 3.0f, 1e-6);
+  EXPECT_NEAR(t[0][0][3], 4.0f, 1e-6);
+
+  t = {{{1.0f, 2.0f, 3.0f, 4.0f}}};
+  pos = 1;
+  rope(t, n_rot, rope_freq_base, rope_freq_scale, pos);
+
+  EXPECT_NEAR(t[0][0][0], -1.984111f, 1e-4);
+  EXPECT_NEAR(t[0][0][2], 2.462337f, 1e-4);
+}
+
+TEST(OpsTest, Scale) {
+  tensor_3 t = {{{1.0f, 2.0f}, {3.0f, 4.0f}}};
+  scale(t, 2.0f);
+  EXPECT_NEAR(t[0][0][0], 2.0f, 1e-6);
+  EXPECT_NEAR(t[0][0][1], 4.0f, 1e-6);
+  EXPECT_NEAR(t[0][1][0], 6.0f, 1e-6);
+  EXPECT_NEAR(t[0][1][1], 8.0f, 1e-6);
 }
