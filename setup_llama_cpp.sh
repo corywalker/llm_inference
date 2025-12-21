c6c5d32336fa9c1e0d423091473a4260e444a945
Cory Walker <cwalker32@gmail.com>
2025-12-20 22:05:47 -0800

    Update build flags.

--- setup_llama_cpp.sh
+++ setup_llama_cpp.sh
@@ -5,7 +5,9 @@ if [ ! -d "./third_party/llama.cpp" ]; then
   git clone https://github.com/ggml-org/llama.cpp.git ./third_party/llama.cpp
 fi
 cd ./third_party/llama.cpp
-cmake -B build_dbg -DCMAKE_BUILD_TYPE=Debug
+
+COMMON_CMAKE_FLAGS="-DGGML_METAL=OFF -DGGML_BLAS=OFF"
+cmake -B build_dbg -DCMAKE_BUILD_TYPE=Debug $COMMON_CMAKE_FLAGS
 cmake --build build_dbg -j 4
-cmake -B build
+cmake -B build $COMMON_CMAKE_FLAGS
 cmake --build build --config Release -j 4
